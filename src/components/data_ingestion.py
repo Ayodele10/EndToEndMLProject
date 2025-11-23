00db44e9198429a574c1b85b012a35e8571aa840
Ayodele10 <egbayeloayodele72@gmail.com>
2025-11-23 18:58:54 +0100

    model trainer done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,12 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+
+
+from src.components.model_trainer import Model_Trainer
+from src.components.model_trainer import ModelTrainerConfig
+
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', 'train.csv')
@@ -50,3 +56,7 @@ if __name__ == "__main__":
 
     data_transformation = DataTransformation()
     train_arr, test_arr, preprocessor_obj = data_transformation.initiate_data_transformation(train_data, test_data)  
+
+
+    modelTrainer = Model_Trainer()
+    print(modelTrainer.initiate_model_trainer(train_arr, test_arr))
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,8 @@ import dill
 from src.exception import CustomException
 from src.logger import logging
 
+from sklearn.metrics import r2_score
+
 
 def save_object(file_path, obj):
     try:
@@ -14,6 +16,33 @@ def save_object(file_path, obj):
 
         with open(file_path, 'wb') as file_obj:
             dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            # Train the model
+            model.fit(X_train, y_train)
+
+            # Predicting the training set
+            y_train_pred = model.predict(X_train)
+
+            # Predicting the test set
+            y_test_pred = model.predict(X_test)
+
+            # R2 Score
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
 
     except Exception as e:
         raise CustomException(e, sys)
